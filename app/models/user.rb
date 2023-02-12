f01f421dd42ceaf2ca91fba92ec829a7de75408e
ziyadtalha <ziyadtalha24@gmail.com>
2023-02-12 21:29:23 +0500

    Fixed: Foreign key violation on Delete User (User was not being deleted when they had followers/following)

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,13 +8,13 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
 
   # Will return an array of follows for the given user instance
-  has_many :received_follows, foreign_key: :followed_id, class_name: "Follow"
+  has_many :received_follows, foreign_key: :followed_id, class_name: "Follow", dependent: :destroy
 
   # Will return an array of users who follow the user instance
   has_many :followers, through: :received_follows, source: :follower
 
   # returns an array of follows a user gave to someone else
-  has_many :given_follows, foreign_key: :follower_id, class_name: "Follow"
+  has_many :given_follows, foreign_key: :follower_id, class_name: "Follow", dependent: :destroy
 
   # returns an array of other users who the user has followed
   has_many :followings, through: :given_follows, source: :followed_user
